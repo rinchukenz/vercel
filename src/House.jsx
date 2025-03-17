dfa528edd53997e07f060df93d33d473cac467f3
rinchukenz <rinchu9999@gmail.com>
2025-03-17 21:15:16 +0530

    changed house.jsx

--- src/House.jsx
+++ src/House.jsx
@@ -5,6 +5,7 @@ function House() {
   return (
     <div className='house'>
       <img src="https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+      <img src="https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
     </div>
   )
 }
